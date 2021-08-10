a0027629cfc6fcee1af0b71cdd003bea257d5e65
Francisco Sabino <fdasabino@gmail.com>
2021-08-10 08:43:11 +0000

    Add sql-expression file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -21,7 +21,8 @@ cursor = connection.cursor()
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# Query 6 - select all tracks where the composer is
+# "Queen" from the "Track" table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fecth the results(multiple)
